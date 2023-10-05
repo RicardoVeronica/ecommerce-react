f31be445185e07380d052f31ef159f8e3555fa72
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-10-04 18:44:12 -0600

    fix: images routes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,11 @@
 import Icon from '../components/icon/Icon'
 
+import aboutImage from '../assets/img/about.jpg'
+import iconOne from '../assets/img/icon_1.png'
+import iconTwo from '../assets/img/icon_2.png'
+import iconThree from '../assets/img/icon_3.png'
+import iconFour from '../assets/img/icon_4.png'
+
 import './about.css'
 
 function About() {
@@ -13,7 +19,7 @@ function About() {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa minima distinctio quod impedit neque possimus repellendus nobis debitis cupiditate dignissimos voluptatibus eum, incidunt quas eos odit! Veritatis, repellendus doloremque!</p>
         </div>
 
-        <img className="about__image" src="../../public/img/nosotros.jpg" alt="About image" />
+        <img className="about__image" src={aboutImage} alt="About image" />
       </section>
 
       <h1>Why Buy With Us</h1>
@@ -21,28 +27,28 @@ function About() {
       <section className="icons">
         <div className="icons__container">
           <Icon
-            image="../../public/img/icono_1.png"
+            image={iconOne}
             image_alt="Money icon"
             title="The Best Price"
             text="lorem ipsum dolor sit."
           />
 
           <Icon
-            image="../../public/img/icono_2.png"
+            image={iconTwo}
             image_alt="HTML5 icon"
             title="For Developers"
             text="lorem ipsum dolor sit."
           />
 
           <Icon
-            image="../../public/img/icono_3.png"
+            image={iconThree}
             image_alt="Shipping icon"
             title="Free Shipping"
             text="lorem ipsum dolor sit."
           />
 
           <Icon
-            image="../../public/img/icono_4.png"
+            image={iconFour}
             image_alt="Quality icon"
             title="Best Quality"
             text="lorem ipsum dolor sit."
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,4 +1,20 @@
 import Product from '../components/product/Product'
+
+import one from '../assets/img/1.jpg'
+import two from '../assets/img/2.jpg'
+import three from '../assets/img/3.jpg'
+import four from '../assets/img/4.jpg'
+import five from '../assets/img/5.jpg'
+import six from '../assets/img/6.jpg'
+import seven from '../assets/img/7.jpg'
+import eight from '../assets/img/8.jpg'
+import nine from '../assets/img/8.jpg'
+import ten from '../assets/img/10.jpg'
+import eleven from '../assets/img/11.jpg'
+import twelve from '../assets/img/12.jpg'
+import thirteen from '../assets/img/13.jpg'
+import fourteen from '../assets/img/14.jpg'
+
 import './store.css'
 
 function Store() {
@@ -8,98 +24,98 @@ function Store() {
 
       <section className="grid">
         <Product
-          image_url="../../../public/img/1.jpg"
+          image_url={one}
           image_alt="VueJS T-Shirt"
           name="Vue"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/2.jpg"
+          image_url={two}
           image_alt="AngularJS T-Shirt"
           name="Angular"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/3.jpg"
+          image_url={three}
           image_alt="ReactJS T-Shirt"
           name="React"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/4.jpg"
+          image_url={four}
           image_alt="Redux T-Shirt"
           name="Redux"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/5.jpg"
+          image_url={five}
           image_alt="NodeJS T-Shirt"
           name="Node"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/6.jpg"
+          image_url={six}
           image_alt="Sass T-Shirt"
           name="Sass"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/7.jpg"
+          image_url={seven}
           image_alt="HTML5 T-Shirt"
           name="HTML5"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/8.jpg"
+          image_url={eight}
           image_alt="GitHub T-Shirt"
           name="GitHub"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/9.jpg"
+          image_url={nine}
           image_alt="BulmaCSS T-Shirt"
           name="BulmaCSS"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/10.jpg"
+          image_url={ten}
           image_alt="TypeScript T-Shirt"
           name="TypeScript"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/11.jpg"
+          image_url={eleven}
           image_alt="Drupal T-Shirt"
           name="Drupal"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/12.jpg"
+          image_url={twelve}
           image_alt="JavaScript T-Shirt"
           name="JavaScript"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/13.jpg"
+          image_url={thirteen}
           image_alt="GraphQL T-Shirt"
           name="GraphQL"
           price="25"
         />
 
         <Product
-          image_url="../../../public/img/14.jpg"
+          image_url={fourteen}
           image_alt="WordPress T-Shirt"
           name="WordPress"
           price="25"
